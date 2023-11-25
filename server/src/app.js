8ced2b631e4d38081b73257ce3dab606422ce0f6
El Samp <anthony.francisco.aoct@gmail.com>
2023-11-24 23:41:35 -0500

    erg

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(rateLimit); // Apply rate limiting as a global middleware
+// app.use(rateLimit); // Apply rate limiting as a global middleware
 
 // Static directory
 app.use('/uploads', express.static('uploads'));
--- server/src/services/pdfService.js
+++ server/src/services/pdfService.js
@@ -1,24 +1,41 @@
-const { simplifyText } = require('./openaiService');
 const fs = require('fs');
 const util = require('util');
 const unlinkFile = util.promisify(fs.unlink);
+const pdfParse = require('pdf-parse');
 
 exports.handlePdfUpload = async (req, res) => {
-  if (!req.file) {
-    return res.status(400).send('No file uploaded.');
-  }
+    console.log("Hello")
+    if (!req.file) {
+        return res.status(400).send('No file uploaded.');
+    }
 
-  try {
-    // Here you would parse the PDF and extract the text
-    const text = '...'; // Placeholder for extracted text
-    const simplifiedText = await simplifyText(text);
+    try {
+        // Read the content of the PDF file
+        const pdfBuffer = fs.readFileSync(req.file.path);
+        console.log("1st step:", pdfBuffer)
 
-    // Delete the uploaded file after processing
-    await unlinkFile(req.file.path);
+        // Parse the PDF and extract the text
+        const data = await pdfParse(pdfBuffer);
+        const extractedText = data.text;
+        console.log("2nd step:", data)
+        console.log("3rd step:", extractedText)
 
-    res.json({ simplifiedText });
-  } catch (error) {
-    console.error('Error processing PDF:', error);
-    res.status(500).send('Error processing PDF.');
-  }
+
+
+        // Delete the uploaded file after processing
+        await unlinkFile(req.file.path);
+        // Send the extracted text back in the response
+        res.json({ text: extractedText });
+    } catch (error) {
+        console.error('Error message:', error.message);
+        console.error('Error stack:', error.stack);
+        console.error('Error processing PDF:', error);
+
+        // Ensure the uploaded file is deleted even if parsing fails
+        if (req.file?.path) {
+            await unlinkFile(req.file.path).catch((err) => console.error('Error deleting file:', err));
+        }
+
+        res.status(500).send('Error processing PDF.');
+    }
 };
